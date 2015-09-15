a7fe87a4f90b95a8cf8902cb5c2547b7bd604700
Adam Williams <awilli1186@gmail.com>
2015-09-15 13:24:06 -0500

    changed loading wheel

--- src/js/components/story-wizard/confirmation.js
+++ src/js/components/story-wizard/confirmation.js
@@ -19,7 +19,7 @@ let Confirmation = React.createClass({
   render: function() {
     let submitText = 'Submit Story';
     if (this.state.loading) {
-      submitText = <img src="/images/4.GIF" alt="Loading..."/>
+      submitText = <img src="/images/ajax-loader.gif" alt="Loading..."/>
     }
     return (
       <div className="confirmation">
